15da0735e763a48185f2fc462975f70387918b4c
Janhvi <Janhvi2013031@akgec.ac.in>
2022-08-29 19:31:55 +0530

    Node callbacks and error handling

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
+var rect=require('./rectangle');
 function solveRect(l,b){
     console.log("Solving for rectangle with l="+l+"and b="+b);
-
-    if(l<=0 || b<=0){
-        console.log("Rectangle Dimension should be greater than zero :l="+l+"and b="+b);
-           }
-                  else{
-            console.log("Area of rectangle is"+rect.area(l,b));
-            console.log("Perimeter of rectangle is"+rect.perimeter(l,b));
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log("ERROR: ", err.message);
         }
-    }
-
-
+        else{
+            console.log("Area of rectangle of dimensions l="+l+",and b="+b+ "is"+rectangle.area());
+            console.log("Perimeter of rectangle of dimensions l="+l+",and b="+b+ "is"+rectangle.perimeter());
+        }
+    });
+    console.log("This staement after the call to rect()");
+}
 solveRect(3,5);
-solveRect(4,3);
+solveRect(6,8);
 solveRect(0,5);
-solveRect(-6,8);
+solveRect(-6,2);
